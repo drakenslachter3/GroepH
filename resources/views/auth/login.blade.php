b9b21b7aea0519eeb79822345dea06951cd08338
Jay Dielemans <Jay.dielemans@student.avans.nl>
2025-03-15 19:59:08 +0100

    Saturday night commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,23 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
+        <div class="flex flex-row justify-between">
+         
+            <!-- Login Button -->
+            <x-primary-button class="mt-4">
                 {{ __('Log in') }}
             </x-primary-button>
+
+            <!-- Forgot Password Link -->
+            @if (Route::has('password.request'))
+                <div class="mt-5 pt-2 text-sm">
+                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                </div>
+            @endif
+
         </div>
+
     </form>
 </x-guest-layout>
