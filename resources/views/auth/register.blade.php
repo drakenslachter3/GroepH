179b0db4c2aa79344ced24ac6c3d309f718b75b5
Jay Dielemans <Jay.dielemans@student.avans.nl>
2025-03-16 22:36:44 +0100

    Small changes to Register form layout
    Changed the form layout for consistency with he Login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,14 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="mt-4">&nbsp;</div>
+
+        <div class="flex items-center justify-between mt-4 mb-4">
+            <x-primary-button>
+                {{ __('Register') }}
+            </x-primary-button>    
+            
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
