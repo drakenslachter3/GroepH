f8328c54b2f34df31914c787b3c0fa3979ae5796
Jay Dielemans <Jay.dielemans@student.avans.nl>
2025-03-19 20:06:40 +0100

    Small visual changes for presentation

--- resources/views/energy/form.blade.php
+++ resources/views/energy/form.blade.php
@@ -13,7 +13,7 @@
                 @endphp
 
                 <div class="mb-4">
-                    <a href="{{ route('dashboard') }}" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200">
+                    <a href="{{ route('dashboard') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-200">
                         Terug naar dashboard
                     </a>
                 </div>
