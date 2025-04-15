4dc7bd7e192abaa2f91e2e74aa974627c0117cb5
drakenslachter3 <l.veldman@student.avans.nl>
2025-04-15 16:45:39 +0200

    Werkende mails en reset

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -12,22 +12,22 @@
 
         <!-- Email Address (readonly) -->
         <div>
-            <x-input-label for="email_display" :value="__('Email')" />
-            <x-text-input id="email_display" class="block mt-1 w-full bg-gray-100 dark:bg-gray-700" type="email" name="email_display" :value="$email" disabled readonly />
+            <x-etc.input-label for="email_display" :value="__('Email')" />
+            <x-etc.text-input id="email_display" class="block mt-1 w-full bg-gray-100 dark:bg-gray-700" type="email" name="email_display" :value="$email" disabled readonly />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('New Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-etc.input-label for="password" :value="__('New Password')" />
+            <x-etc.text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            <x-etc.input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-etc.input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-etc.text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            <x-etc.input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
