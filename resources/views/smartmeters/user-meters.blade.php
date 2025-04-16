0d9391f4c94f090473203bc30d88ba3311dc90a9
Noah de Groot <n.degroot9@student.avans.nl>
2025-04-16 19:21:13 +0200

    fix meter load

--- resources/views/smartmeters/user-meters.blade.php
+++ resources/views/smartmeters/user-meters.blade.php
@@ -107,7 +107,7 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                                 </svg>
                                             </a>
-                                            <form method="POST" action="{{ route('smartmeters.unlinkMeter', ['user' => $user->id, 'smartMeter' => $meter->id]) }}">
+                                            <form method="POST" action="{{ route('smartmeters.unlinkMeter', ['user' => $user->id, 'smartmeter' => $meter->id]) }}">
                                                 @csrf
                                                 <button type="submit" onclick="return confirm('Weet je zeker dat je deze meter wilt ontkoppelen van de gebruiker?');" class="px-3 py-1 text-red-500 hover:text-red-700">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
