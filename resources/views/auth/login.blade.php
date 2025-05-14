6a820a2a4957ef1f15651302529f569f4b494fe3
Charaf Oualid <542269@student.fontys.nl>
2025-05-14 12:06:51 +0200

    Accessibility: Forms and Interaction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,11 @@
                         autocomplete="username"
                         aria-describedby="email-error"
                     />
-                    <x-etc.input-error :messages="$errors->get('email')" class="mt-2" id="email-error" />
+                    @if ($errors->has('email'))
+                        <div id="email-error" class="mt-2 text-sm text-red-600 dark:text-red-400" role="alert">
+                            {{ $errors->first('email') }}
+                        </div>
+                    @endif
                 </div>
 
                 <!-- Wachtwoord -->
@@ -81,7 +85,11 @@
                                 d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                         </svg>
                     </div>
-                    <x-etc.input-error :messages="$errors->get('password')" class="mt-2" id="password-error" />
+                    @if ($errors->has('password'))
+                        <div id="password-error" class="mt-2 text-sm text-red-600 dark:text-red-400" role="alert">
+                            {{ $errors->first('password') }}
+                        </div>
+                    @endif
                 </div>
 
                 <!-- Gegevens onthouden -->
@@ -103,7 +111,7 @@
                     <div class="flex flex-row justify-between items-center flex-wrap gap-4 mt-4">
 
                         <!-- Login Button -->
-                        <x-primary-button>
+                        <x-primary-button type="submit">
                             {{ __('Log in') }}
                         </x-primary-button>
 
