068a890559819101678f2b5f203bf31634368af0
Noah de Groot <n.degroot9@student.avans.nl>
2025-06-26 12:23:21 +0200

    dark mode fix

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,14 +9,14 @@
     <div class="max-w-4xl mx-auto px-4 py-8">
         {{-- Success Message --}}
         @if(session('success'))
-            <div class="mb-6 bg-green-900 border border-green-700 rounded-lg p-4">
+            <div class="mb-6 bg-green-100 border border-green-500 rounded-lg p-4 dark:bg-green-900 dark:border-green-700">
                 <div class="flex items-center">
-                    <svg class="h-5 w-5 text-green-400 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                    <svg class="h-5 w-5 text-green-500 dark:text-green-400 mr-2" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                             d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <p class="text-green-300">{{ session('success') }}</p>
+                    <p class="text-green-800 dark:text-green-300">{{ session('success') }}</p>
                 </div>
             </div>
         @endif
